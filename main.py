c50dc64feaf6720ea49604227d9e6e9ef9fa69e3
raianecaroline <raicaroline674@gmail.com>
2022-08-22 10:08:26 -0300

    criei o crud

--- main.py
+++ main.py
@@ -9,30 +9,30 @@ conexao = mysql.connector.connect(
 cursor = conexao.cursor()
 
 #CREATE
-#nome_produto = "refrigerante"
-#valor_produto = 5
-#comando = f'INSERT INTO vendas (nome_produto, valor_produto) VALUES ("{nome_produto}", "{valor_produto}")'
-#cursor.execute(comando)
-#conexao.commit() # quando edita o banco de dados
+nome_produto = "refrigerante"
+valor_produto = 5
+comando = f'INSERT INTO vendas (nome_produto, valor_produto) VALUES ("{nome_produto}", "{valor_produto}")'
+cursor.execute(comando)
+conexao.commit()
 
 #READ
-#comando = f'SELECT * FROM vendas'
-#cursor.execute(comando)
-#resultado = cursor.fetchall() # ler o banco de dados
-#print(resultado)
+comando = f'SELECT * FROM vendas'
+cursor.execute(comando)
+resultado = cursor.fetchall()
+print(resultado)
 
 #UPDATE
-#nome_produto = "refrigerante"
-#valor_produto = 10
-#comando = f'UPDATE vendas SET valor_produto = {valor_produto} WHERE nome_produto = "{nome_produto}"'
-#cursor.execute(comando)
-#conexao.commit() # quando edita o banco de dados
+nome_produto = "refrigerante"
+valor_produto = 10
+comando = f'UPDATE vendas SET valor_produto = {valor_produto} WHERE nome_produto = "{nome_produto}"'
+cursor.execute(comando)
+conexao.commit()
 
 #DELETE
-#nome_produto = "refrigerante"
-#comando = f'DELETE vendas WHERE nome_produto = "{nome_produto}"'
-#cursor.execute(comando)
-#conexao.commit() # quando edita o banco de dados
+nome_produto = "refrigerante"
+comando = f'DELETE vendas WHERE nome_produto = "{nome_produto}"'
+cursor.execute(comando)
+conexao.commit()
 
 cursor.close()
 conexao.close()
